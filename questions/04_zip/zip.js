247de59d54563d20c5a24b28c38405d29dcbcf8d
Alexandra Morozoff <a.j.morozoff@gmail.com>
2019-10-02 18:55:36 -0400

    stretch

--- questions/04_zip/zip.js
+++ questions/04_zip/zip.js
@@ -1,3 +1,4 @@
+/* eslint-disable guard-for-in */
 /*
 write the zip function
 zip([{x: 1}, {x: 2}]) => {x: 3}
@@ -7,6 +8,19 @@ zip([
   {z: 2, y: 1}]) => {x: 3, y: 3, z: 3}
 */
 
-function zip(objs) {}
+function zip(objs) {
+  let zipped = {};
+  for (let obj in arguments) {
+    Object.keys(obj).forEach(key => {
+      if (zipped[key]) {
+        zipped[key] += obj[key];
+      }
+      else {
+        zipped[key] = obj[key];
+      }
+    })
+  }
+  return zipped;
+}
 
 module.exports = { zip };
