7f44a9d4f5b7951c21572b6eba30e6a60e8773a1
Alexandra Morozoff <a.j.morozoff@gmail.com>
2019-10-09 18:56:19 -0400

    stretch

--- questions/06_times/times.js
+++ questions/06_times/times.js
@@ -1,3 +1,15 @@
-function times() {}
+function times(n) {
+    try {
+        if (typeof n != 'number') {
+            throw 'Numbers only!';
+        }
+        return function(m) {
+            return m * n;
+        }
+    }
+    catch (e) {
+        return e;
+    }
+}
 
 module.exports = { times };
