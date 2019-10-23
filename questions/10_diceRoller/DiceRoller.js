901730afc776dda39ec2344bcfcaf58cc935729c
Alexandra Morozoff <a.j.morozoff@gmail.com>
2019-10-23 18:57:54 -0400

    stretch

--- questions/10_diceRoller/DiceRoller.js
+++ questions/10_diceRoller/DiceRoller.js
@@ -4,6 +4,30 @@
 
 class DiceRoller {
   //YOUR CODE
+  constructor(sides, total) {
+    if (sides < 1 || total < 1 || typeof sides != 'number' || typeof total != 'number') {
+      throw('invalid sides or total');
+    }
+    this.sides = sides;
+    this.total = total;
+    this.history = [];
+  }
+  roll() {
+    let rolls = [];
+    let count = this.total;
+    while (count > 0) {
+      let roll = Math.ceil(Math.random() * this.sides);
+      //console.log(roll);
+      rolls.push(roll);
+      --count;
+    }
+    this.history.push(rolls);
+    return rolls;
+  }
+
+
 }
+
+
 
 module.exports = { DiceRoller };
