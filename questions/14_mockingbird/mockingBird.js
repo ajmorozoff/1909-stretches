d4db17f61ca8d3c549b0d186e5d22fb9abd94d8f
Alexandra Morozoff <a.j.morozoff@gmail.com>
2019-11-11 18:51:23 -0500

    solution

--- questions/14_mockingbird/mockingBird.js
+++ questions/14_mockingbird/mockingBird.js
@@ -1,5 +1,12 @@
 const repeater = (func, n) => {
   // repeater runs a function n times and returns an array of its outputs
+  let arr = [];
+  while (n > 0) {
+    const result = func();
+    arr.push(result);
+    --n;
+  }
+  return arr;
 };
 
 module.exports = { repeater };
