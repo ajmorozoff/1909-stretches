6c85477e60ee2002ab3297cc940b1e00cbfab144
Alexandra Morozoff <a.j.morozoff@gmail.com>
2019-11-13 18:56:32 -0500

    stretch

--- questions/15_cache/cache.js
+++ questions/15_cache/cache.js
@@ -1,3 +1,18 @@
-function cache() {}
-
+function cache(func) {
+    if (typeof func !== 'function') {
+        throw('Input must be a function.');
+    }
+    let argumentCache = [];
+    return function(arg) {
+        let obj = argumentCache.find(obj => obj.passed === arg)
+        if (obj) {
+            return obj.result;
+        }
+        else {
+            let res = func(arg);
+            argumentCache.push({passed: arg, result: res});
+            return res;
+        }
+    }
+}
 module.exports = { cache };
