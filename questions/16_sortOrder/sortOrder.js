11caee3ba6b368c95837af6a0be1683f848630d6
Alexandra Morozoff <a.j.morozoff@gmail.com>
2019-11-18 18:56:51 -0500

    stretch

--- questions/16_sortOrder/sortOrder.js
+++ questions/16_sortOrder/sortOrder.js
@@ -3,6 +3,11 @@ SortOrder takes an array of objects, a key to be sorted on
 and a key for the return value. 
 */
 
-function sortOrder() {}
+function sortOrder(arr, sortKey, returnKey) {
+    let sorted = [];
+    arr.sort((a, b) => {return a[sortKey] < b[sortKey] ? -1 : 1});
+    arr.forEach(obj => sorted.push(obj[returnKey]));
+    return sorted;
+}
 
 module.exports = { sortOrder };
