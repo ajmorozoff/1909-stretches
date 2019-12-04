5555e86504514e9073c6397498d671c0d23e6efb
Alexandra Morozoff <a.j.morozoff@gmail.com>
2019-12-04 18:56:35 -0500

    stretch

--- questions/19_calcluator/calculator.js
+++ questions/19_calcluator/calculator.js
@@ -1,3 +1,32 @@
-class Calculator {}
+class Calculator {
+    constructor() {
+       this.value = 0;
+    }
+
+
+    add(n) {
+        this.value += n;
+        return this;
+    }
+
+    subtract(n) {
+        this.value -= n;
+        return this;
+    }
+    divide(n) {
+        if (n !== 0) {
+            this.value = this.value / n;
+        }
+        return this;
+    }
+    multiply(n) {
+        this.value = this.value * n;
+        return this;
+    }
+    clear() {
+        this.value = 0;
+        return this;
+    }
+}
 
 module.exports = { Calculator };
